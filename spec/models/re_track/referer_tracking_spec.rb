cbd28beb2a2a0000b26a035ad3ce268ccf812c1f
Gebhard Wöstemeyer <g.woestemeyer@gmail.com>
2013-06-13 16:17:08 +0200

    spec

--- spec/models/re_track/referer_tracking_spec.rb
+++ spec/models/re_track/referer_tracking_spec.rb
@@ -5,58 +5,60 @@ module ReTrack
   describe RefererTracking do
     let(:referer_tracking) { RefererTracking.new }
 
-    subject { referer_tracking.query('q') }
+    describe '#query' do
+      subject { referer_tracking.query('q') }
 
-    it { should be_nil }
-
-    context 'when there is a first_url' do
-      before { referer_tracking.first_url = 'http://google.de/' }
       it { should be_nil }
-    end
 
-    context 'when the first_url contains the parameter' do
-      before { referer_tracking.first_url = 'http://google.de/?q=query&a=b' }
-      it { should == 'query' }
-    end
-
-    context 'when the first_url does not contain the parameter' do
-      before { referer_tracking.first_url = 'http://google.de/?a=b' }
-      it { should be_nil }
-    end
-
-    context 'when first_url is url_encoded' do
-      before do
-        referer_tracking.first_url = CGI::escape 'http://google.de/?q=query'
+      context 'when there is a first_url' do
+        before { referer_tracking.first_url = 'http://google.de/' }
+        it { should be_nil }
       end
-      it { should == 'query' }
-    end
 
-    context 'with an invalid url' do
-      before { referer_tracking.first_url = 'h a l l o' }
-      it { should be_nil }
-    end
-
-    context 'when querying an invalid field' do
-      before do
-        referer_tracking.first_url = '?q=f'
-        referer_tracking.user_agent = '?q=u'
+      context 'when the first_url contains the parameter' do
+        before { referer_tracking.first_url = 'http://google.de/?q=query&a=b' }
+        it { should == 'query' }
       end
-      subject { referer_tracking.query('q', 'user_agent') }
-      it { should be_nil }
-    end
 
-    context 'when querying a non-existent field' do
-      subject { referer_tracking.query('q', 'hidden') }
-      it { should be_nil }
-    end
-
-    context 'when querying referer_url' do
-      before do
-        referer_tracking.first_url = '?q=f'
-        referer_tracking.referer_url = '?q=r'
+      context 'when the first_url does not contain the parameter' do
+        before { referer_tracking.first_url = 'http://google.de/?a=b' }
+        it { should be_nil }
       end
-      subject { referer_tracking.query('q', 'referer_url') }
-      it { should == 'r' }
+
+      context 'when first_url is url_encoded' do
+        before do
+          referer_tracking.first_url = CGI::escape 'http://google.de/?q=query'
+        end
+        it { should == 'query' }
+      end
+
+      context 'with an invalid url' do
+        before { referer_tracking.first_url = 'h a l l o' }
+        it { should be_nil }
+      end
+
+      context 'when querying an invalid field' do
+        before do
+          referer_tracking.first_url = '?q=f'
+          referer_tracking.user_agent = '?q=u'
+        end
+        subject { referer_tracking.query('q', 'user_agent') }
+        it { should be_nil }
+      end
+
+      context 'when querying a non-existent field' do
+        subject { referer_tracking.query('q', 'hidden') }
+        it { should be_nil }
+      end
+
+      context 'when querying referer_url' do
+        before do
+          referer_tracking.first_url = '?q=f'
+          referer_tracking.referer_url = '?q=r'
+        end
+        subject { referer_tracking.query('q', 'referer_url') }
+        it { should == 'r' }
+      end
     end
   end
 end
